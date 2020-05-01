b2046dd062351d6e413962c7d0355f0302132ec0
John William Kustin <kustinj@stanford.edu>
2020-05-01 14:46:40 -0700

    rename

--- datasets/download-sample.sh
+++ datasets/download-sample.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cat "Downloading Deepfake face dataset sample"
+echo "Downloading Deepfake face dataset sample"
 grep sample dataset-paths.csv | while read line
 do
     kaggle datasets download $line
