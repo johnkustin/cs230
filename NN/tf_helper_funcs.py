67361c823c82d339f349bd7e8c092addeac6f48b
Isaac Schaider <ischaider@Isaacs-MacBook-Pro.local>
2020-05-05 22:22:40 -0500

    tensorflow files -- they should work but not sure lmk

---
+++ NN/tf_helper_funcs.py
@@ -0,0 +1,103 @@
+def create_placeholders(n_x, n_y):
+    """
+    Creates the placeholders for the tensorflow session.
+    The total dataset is 8201 examples but the last 500 are reserved for the test data 
+    X_train shape: (76800, 7701) 
+    Y_train shape: (1, 7701)
+    
+    Arguments:
+    n_x -- scalar, size of an image vector (num_px * num_px = 160 * 160 * 3 = 76800)
+    n_y -- scalar, number of classes
+    
+    Returns:
+    X -- placeholder for the data input, of shape [n_x, None] and dtype "tf.float32"
+    Y -- placeholder for the input labels, of shape [n_y, None] and dtype "tf.float32"
+    """
+
+    X = tf.placeholder(dtype=tf.float32, shape=[n_x, None])
+    Y = tf.placeholder(dtype=tf.float32, shape=[n_y, None])
+    
+    return X, Y
+
+
+def initialize_parameters():
+    """
+    Initializes parameters to build a neural network with tensorflow. The shapes are:
+                        W1 : [25, 76800]
+                        b1 : [25, 1]
+                        W2 : [12, 25]
+                        b2 : [12, 1]
+                        W3 : [1, 12]
+                        b3 : [1, 1]
+    
+    Returns:
+    parameters -- a dictionary of tensors containing W1, b1, W2, b2, W3, b3
+    """
+    
+    tf.set_random_seed(1)                  
+        
+    W1 = tf.get_variable("W1", [25,76800], initializer = tf.contrib.layers.xavier_initializer(seed = 1))
+    b1 = tf.get_variable("b1", [25,1], initializer = tf.zeros_initializer())
+    W2 = tf.get_variable("W2", [12, 25], initializer = tf.contrib.layers.xavier_initializer(seed = 1))
+    b2 = tf.get_variable("b2", [12, 1], initializer = tf.zeros_initializer())
+    W3 = tf.get_variable("W3", [1, 12], initializer = tf.contrib.layers.xavier_initializer(seed = 1))
+    b3 = tf.get_variable("b3", [1, 1], initializer = tf.zeros_initializer())
+
+    parameters = {"W1": W1,
+                  "b1": b1,
+                  "W2": W2,
+                  "b2": b2,
+                  "W3": W3,
+                  "b3": b3}
+    
+    return parameters
+
+
+def forward_propagation(X, parameters):
+    """
+    Implements the forward propagation for the model: LINEAR -> RELU -> LINEAR -> RELU -> LINEAR -> SIGMOID
+    
+    Arguments:
+    X -- input dataset placeholder, of shape (input size, number of examples)
+    parameters -- python dictionary containing your parameters "W1", "b1", "W2", "b2", "W3", "b3"
+                  the shapes are given in initialize_parameters
+
+    Returns:
+    Z3 -- the output of the last LINEAR unit
+    """
+    
+    # Retrieve the parameters from the dictionary "parameters" 
+    W1 = parameters['W1']
+    b1 = parameters['b1']
+    W2 = parameters['W2']
+    b2 = parameters['b2']
+    W3 = parameters['W3']
+    b3 = parameters['b3']
+                
+    Z1 = tf.add(tf.matmul(parameters["W1"], X), b1)
+    A1 = tf.nn.relu(Z1)                                              
+    Z2 = tf.add(tf.matmul(parameters["W2"], A1), b2)
+    A2 = tf.nn.relu(Z2)                                             
+    Z3 = tf.add(tf.matmul(parameters["W3"], A2), b3)      
+    
+    return Z3
+
+
+def compute_cost(Z3, Y):
+    """
+    Computes the cost
+    
+    Arguments:
+    Z3 -- output of forward propagation (output of the last LINEAR unit)
+    Y -- "true" labels vector placeholder, same shape as Z3
+    
+    Returns:
+    cost - Tensor of the cost function
+    """
+    
+    logits = tf.transpose(Z3)
+    labels = tf.transpose(Y)
+    
+    cost = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits = logits, labels = labels))
+    
+    return cost
